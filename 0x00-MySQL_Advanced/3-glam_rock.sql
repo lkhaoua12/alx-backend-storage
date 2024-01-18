24465db7e07d81f6f77e59fbb9882bf78e90ef06
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2024-01-18 20:07:52 +0100

    a SQL script that lists all bands with Glam rock as their main style, ranked by their longevity

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,8 @@
+-- SQL script that lists all bands with Glam rock as their main style
+SELECT
+    band_name,
+    (IFNULL(split, '2022') - formed) AS longevity
+FROM
+    metal_bands
+WHERE
+    style LIKE '%Glam rock%';
