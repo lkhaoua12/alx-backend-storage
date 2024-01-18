ebc5c080de5b6eb3a84f2f0592c26d102f06d49d
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2024-01-18 14:30:55 +0100

    fixed name not null

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,7 @@
 -- Create a table with unique email addresses
-CREATE TABLE IF NOT EXISTS users(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255) NOT NULL
-);
+CREATE TABLE
+    IF NOT EXISTS users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        name VARCHAR(255)
+    );
