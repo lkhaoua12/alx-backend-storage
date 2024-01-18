a0da7c35bace12b1a3d5987b76963a65dd436d6e
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2024-01-18 20:11:12 +0100

    fixed desc order

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,10 @@
 -- SQL script that lists all bands with Glam rock as their main style
 SELECT
     band_name,
-    (IFNULL(split, 2022) - formed) AS longevity
+    (IFNULL(split, '2022') - formed) AS lifespan
 FROM
     metal_bands
 WHERE
     style LIKE '%Glam rock%'
 ORDER BY
-    longevity DESC;
+    lifespan DESC;
