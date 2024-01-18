af91efc4532e5108d2801941e7d3c51601cfc936
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2024-01-18 20:27:44 +0100

    fixed new keyword

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,13 +1,12 @@
---  a SQL script that creates a trigger that resets the attribute valid_email.
-DELIMITER / / CREATE TRIGGER check_valid_email
-AFTER
-UPDATE
-    ON users FOR EACH ROW BEGIN IF NEW.email != OLD.email THEN
-SET
-    NEW.valid_email = 0;
-
-END IF;
-
+-- Trigger to reset valid_email when email is changed
+DELIMITER //
+CREATE TRIGGER reset_valid_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email != OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
 END;
-
-/ / DELIMITER;
+//
+DELIMITER ;
