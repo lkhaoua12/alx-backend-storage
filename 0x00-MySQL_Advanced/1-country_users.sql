f8a3c7aa15bb732f31d84eac2347e5c8e779c196
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2024-01-18 14:44:33 +0100

    fixed syntax in uppercase

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
--- Create a table with unique email addresses
+-- 1. Create a table called users with the following columns:
 CREATE TABLE
     IF NOT EXISTS users (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        email VARCHAR(255) UNIQUE NOT NULL,
+        email VARCHAR(255) NOT NULL UNIQUE,
         name VARCHAR(255),
-        country ENUM ('US', 'CA', 'MX') NOT NULL DEFAULT 'US'
+        country ENUM ('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
     );
