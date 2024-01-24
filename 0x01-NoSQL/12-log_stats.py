135f5ec5ae8314045251c2a3b30ae937fa1137fb
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2024-01-24 01:07:55 +0100

    provides some stats about Nginx logs stored in MongoDB

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -15,6 +15,10 @@ def display_stat():
         method_count = logs_collection.count_documents({"method": method})
         print("\t method {}: {}".format(method, method_count))
 
+    status_check = logs_collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print("{} status check".format(status_check))
+
 
 if __name__ == "__main__":
     display_stat()
