3901802416f2ac1db9d82f66ea42903f0b6002db
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2024-01-24 01:03:03 +0100

    provides some stats about Nginx logs stored in MongoDB

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" provides some stats about Nginx logs stored in MongoDB """
+from pymongo import MongoClient
+
+
+def display_stat():
+    """ provides some stats about Nginx logs stored in MongoDB """
+    client = MongoClient('mongodb://127.0.0.1:27017/')
+    logs_collection = client.logs.nginx
+    count = logs_collection.count_documents({})
+    print("{} logs".format(count))
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        method_count = logs_collection.count_documents({"method": method})
+        print("\t method {}: {}".format(method, method_count))
+
+
+if __name__ == "__main__":
+    display_stat()
