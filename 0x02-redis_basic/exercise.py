0da8747b556eb548fa23f82b53e83c0029fde273
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2024-01-25 00:05:06 +0100

    fix indent code

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,6 +5,16 @@ create a cach class and insert data with key
 from typing import Callable, Union
 import redis
 import uuid
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache():
@@ -15,6 +25,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ store data and returns key """
         key = str(uuid.uuid4())
