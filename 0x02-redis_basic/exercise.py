f76134e08885dcdb5db874462067371751c109fa
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2024-01-24 23:59:05 +0100

    added get and get_str methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """
 create a cach class and insert data with key
 """
-from typing import Union
+from typing import Callable, Union
 import redis
 import uuid
 
@@ -20,3 +20,33 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int]:
+        """ store data and returns key """
+        data = self._redis.get(key)
+        if data is not None and fn is not None:
+            return fn(data)
+        return data
+    
+    def get_str(self, key: str) -> str:
+        """ get string """
+        return self.get(key, fn = lambda d: d.decode("utf-8"))
+    
+    def get_int(self, key: str) -> int:
+        """ get int """
+        return self.get(key, fn = int)
+    
+
+
+if __name__ == "__main__":
+    cache = Cache()
+
+    TEST_CASES = {
+            b"foo": None,
+            123: int,
+            "bar": lambda d: d.decode("utf-8")
+    }
+
+    for value, fn in TEST_CASES.items():
+        key = cache.store(value)
+        assert cache.get(key, fn=fn) == value
